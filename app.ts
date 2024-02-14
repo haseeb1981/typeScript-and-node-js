e2c319c2030a5de963973330396d2abab4f84078
haseeb1981 <160045261+haseeb1981@users.noreply.github.com>
2024-02-14 23:43:14 +0500

    seven file  add create what is your name browser
    seven file  add create what is your name in browser

---
+++ app.ts
@@ -0,0 +1,9 @@
+let personalName :string ='';
+
+personalName = prompt('what is you name?')|| '';
+
+if(personalName !== null && personalName !== ''){
+    alert(`hello ${personalName},would you like learn some  python today?`)
+}
+else{
+alert} ('you  have to fill your name!')
